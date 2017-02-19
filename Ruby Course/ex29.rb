2eda336684fb7fdc3ce591d23e6169fc421568ce
Phil McVay <phil.mcvay90@gmail.com>
2017-02-19 21:48:18 +0000

    completed exercise 29

---
+++ Ruby Course/ex29.rb
@@ -0,0 +1,30 @@
+people = 100
+cats = 40
+dogs = 27
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world if dry!"
+end
+
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs"
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs"
+end
+
+if people == dogs
+  puts "People are dogs"
+end
